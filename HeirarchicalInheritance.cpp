59adb252ddbeddc5613ffcf6dc25037a1e8a2be9
SUJAN SHRESTHA <61812195+darkweb19@users.noreply.github.com>
2022-03-28 18:52:51 +0545

    Add files via upload

---
+++ HeirarchicalInheritance.cpp
@@ -0,0 +1,42 @@
+//....HEIRARCHIHCAL INHERITANCE....//
+
+#include <iostream>
+using namespace std;
+class A // base classs
+{
+public:
+    void func1()
+    {
+        cout << "Function 1..." << endl;
+    }
+};
+class B : public A // first derieved class from A...
+{
+public:
+    void func2()
+    {
+        cout << "Function 2...." << endl;
+    }
+};
+class C : public A // second derived  class from A
+{
+public:
+    void func3()
+    {
+        cout << "Function 3..." << endl;
+    }
+};
+// Heirarchical Inheritance .....
+int main()
+{
+    cout << "For Class A..." << endl;
+    C c1;
+    c1.func1();
+    // c1.func2();       // cannot be used
+    c1.func3();
+    cout << ".....For Class B" << endl;
+    B b1;
+    b1.func1();
+    b1.func2();
+    return 0;
+}
